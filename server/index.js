99fb2e4ab28b96b2da24fe5417ac1fe5721f13ad
Ravinder Gajulapally <gajulapallyajay@gmail.com>
2023-09-23 11:08:35 -0400

    Retest-1

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ var express = require("express");
 
 var app = express();
 
+app.get('/', (req,res) => {
+    res.json({message:"Hello!!"})
+});
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello word for backend!" });
 });
