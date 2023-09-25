7cd2def37af1e499fbaf061899655183ad48bdf1
Braxton Aubry Reichard <breichar@iu.edu>
2023-09-25 00:42:26 -0400

    frontend setup

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
-var express = require("express");
+const path = require('path');
+const express = require('express');
 
+const PORT = process.env.PORT || 8080;
 var app = express();
 
-app.get('/', (req,res) => {
-    res.json({message:"Hello!!"})
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
+
+
+app.use(
+    express.static(path.resolve(__dirname, '../venuemanagement/build')));
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello word for backend!" });
 });
-
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../venuemanagement/build', 'index.html'));
+});
 module.exports = app;
