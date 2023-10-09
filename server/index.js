e7bb4c48df747c2aaaef74075a34166a4f77a21b
Ravinder Gajulapally <gajulapallyajay@gmail.com>
2023-10-09 19:42:26 -0400

    Sprint-2-Week-1-change

--- server/index.js
+++ server/index.js
@@ -108,4 +108,9 @@ app.post('/api/reset-password',async (req,res)=>{
     }
 });
 
+const PORT = process.env.PORT ||8080;
+
+app.listen(PORT, ()=>{
+    console.log(`Server listening on ${PORT}`);
+})
 module.exports = app;
