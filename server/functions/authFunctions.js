18fc1404f39b6915816eb527f783c9ccf20ae256
Ravinder Gajulapally <gajulapallyajay@gmail.com>
2023-10-09 10:05:14 -0400

    Sprint2-Week1

---
+++ server/functions/authFunctions.js
@@ -0,0 +1,84 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const db = require('../database');
+
+require('dotenv').config()
+
+// Function for user registration
+async function registerUser({ username, email, password, role }) {
+  
+    try {
+    
+    const [existingUser] = await db.promise().query('SELECT * FROM ignite.User WHERE username = ? OR email = ?', [username, email]);
+
+    if (existingUser.length > 0){
+        throw new Error('Username or email already exists');
+    }else if (!username || !email || !password || !role) {
+        throw new Error('Registration details cannot be null');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    const [result] = await db.promise().query('INSERT INTO ignite.User (username, email, password_hash, role) VALUES ( ?, ?, ?, ?)', [ username, email, hashedPassword, role]);
+
+    const [newUser] = await db.promise().query('SELECT * FROM ignite.User WHERE user_id = ?', [result.insertId]);
+
+    // console.log(newUser)
+    
+    const token = jwt.sign({ userId: newUser[0].id, username: newUser[0].username }, process.env.ENV_SECRET_KEY, { expiresIn: '15m' });
+    // console.log(token)
+
+    const [res] = await db.promise().query('INSERT INTO ignite.Tokens (userId, tokenId) VALUES ( ?, ?)',[newUser[0].user_id, token]);
+
+    return { token, registeredUser: newUser[0] };
+    
+  } catch (error) {
+
+    throw error;
+
+  }
+}
+
+// Function for user login
+async function loginUser({ username, password }) {
+  try {
+    const [users] = await db.promise().query('SELECT * FROM ignite.User WHERE username = ?', [username]);
+
+    if (users.length === 0) {
+      throw new Error('User not found');
+    }
+    if (!username || !password) {
+        throw new Error('Username or Password cannot be null');
+    }
+
+
+    const user = users[0];
+
+    const passwordMatch = await bcrypt.compare(password, user.password_hash);
+
+    if (!passwordMatch) {
+      throw new Error('Invalid password');
+    }
+
+    const token = jwt.sign({ userId: user.user_id, username: user.username }, process.env.ENV_SECRET_KEY, { expiresIn: '15m' });
+
+    const [res] = await db.promise().query('SELECT * FROM ignite.Tokens WHERE userId = ?',[user.user_id]);
+
+    if (res.length == 0){
+        await db.promise().query('INSERT INTO ignite.Tokens (userId, tokenId) VALUES ( ?, ?)',[user.user_id,token]);
+    }
+
+    return {token, user};
+
+  } catch (error) {
+
+    throw error;
+
+  }
+}
+
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
--- server/index.js
+++ server/index.js
@@ -1,13 +1,98 @@
 var express = require("express");
 
 var app = express();
+const {registerUser, loginUser} = require('./functions/authFunctions')
+const authenticate = require('./middleware/authMiddleware');
+const {generatePasswordResetToken, sendPasswordResetEmail} = require('./functions/passwordReset')
 
-app.get('/', (req,res) => {
-    res.json({message:"Hello!!"})
+// change this according to the request you make for form parsing use: 
+// app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json()); 
+
+const db = require('./database')
+
+db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err);
+      throw err;
+    }
+    console.log('Connected to MySQL database');
 });
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello word for backend!" });
+
+const usersRoutes = require('./routes/userRoutes');
+
+app.get('/', (req,res) => {
+    res.json({message:"You are at home page!"})
+});
+
+app.use('/api/user', authenticate, usersRoutes);
+
+// Registration route
+app.post('/api/register', async (req, res) => {
+    
+    try {
+      const { username, email, password, role } = req.body;
+      const {token, registeredUser} = await registerUser({ username, email, password, role });
+    //   console.log(token)
+    //   console.log(registeredUser)
+      res.header('Authorization', `Bearer ${token}`);
+      res.status(201).json({user: registeredUser, msg: "Registration Successful"});
+
+    } catch (error) {
+      console.error('Registration error:', error);
+      res.status(500).json({ error: 'Registration failed' });
+
+    }
+});
+
+// Login Route
+app.post('/api/login', async (req, res) => {
+    try {
+      const { username, password } = req.body;
+  
+      const {token, user} = await loginUser({ username, password });
+  
+      if (token) {
+        
+        res.header('Authorization', `Bearer ${token}`);
+        res.status(200).json({user:user});
+
+      } else {
+        res.status(401).json({ error: 'Invalid credentials' });
+      }
+    } catch (error) {
+      console.error('Login error:', error);
+      res.status(500).json({ error: 'Login failed' });
+    }
+});
+
+app.post('/api/forgot-password',async (req,res)=>{
+    try{
+        const {email} = req.body;
+        const passwordResetToken = await generatePasswordResetToken({email});
+        if (passwordResetToken == null){
+            res.status(500).json({msg: "No user Exists with this email"});
+        }else{
+            const passwordResetLink = req.protocol + '://' + req.get('host') + "/reset-password?token="+passwordResetToken;
+            sendPasswordResetEmail(email, passwordResetLink);
+
+            res.status(200).json({ message: 'Password reset email sent' });
+        }
+
+    }catch(error){
+        res.status(500).json({msg:'internal server error'});
+    }
+
+});
+
+app.post('/api/reset-password',(req,res)=>{
+    try{
+       
+    }catch(error){
+        res.status(500).json({msg:'internal server error'});
+    }
 });
 
 module.exports = app;
