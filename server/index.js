6c03d67291bd76393a87ab6321301fcb1c16f0f6
Ravinder Gajulapally <gajulapallyajay@gmail.com>
2023-10-27 23:47:35 -0400

    Sprint_3_Oauth_upcoming_editvenue

--- server/index.js
+++ server/index.js
@@ -1,19 +1,27 @@
 const path = require('path');
 const express = require("express");
-
+const session = require('express-session');
+var passport = require('passport');
 const PORT = process.env.PORT || 8080;
 var app = express();
 
+require('dotenv').config()
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
 
-app.use(
-    express.static(path.resolve(__dirname, '../venuemanagement/build')));
+app.use(session({
+  secret: process.env.ENV_SESSION_KEY, 
+  resave: false,
+  saveUninitialized: true
+}));
 
-app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../venuemanagement/build', 'index.html'));
-    });
+// app.use(
+//     express.static(path.resolve(__dirname, '../venuemanagement/build')));
+
+// app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, '../venuemanagement/build', 'index.html'));
+//     });
 
 const {registerUser, loginUser} = require('./functions/authFunctions')
 const authenticate = require('./middleware/authMiddleware');
@@ -23,8 +31,17 @@ const {twoFactoredMail, verifyTwoFactored} = require('./functions/twoFactoredAut
 // change this according to the request you make for form parsing use: 
 // app.use(express.urlencoded({ extended: true }));
 
+const {oauthTokenize} = require('./functions/authenticateOauth');
 app.use(express.json()); 
-
+app.use(passport.initialize())
+app.use(passport.session());
+app.get('/google', passport.authenticate('google',{scope:['profile', 'email']}));
+app.get('/oauth/google',passport.authenticate('google', { failureRedirect: '/login' }), async (req,res)=>{
+  email = req.user._json.email;
+  const {token,user} = await oauthTokenize({email});
+  res.header('Authorization', `Bearer ${token}`);
+  res.status(200).json({user:user, token: token});
+})
 const db = require('./database')
 
 db.connect((err) => {
@@ -39,7 +56,7 @@ db.connect((err) => {
 const usersRoutes = require('./routes/userRoutes');
 
 app.get('/', (req,res) => {
-    res.json({message:"You are at home page!"})
+    res.json({message:"You are at home page!"});
 });
 
 app.use('/api/user', authenticate, usersRoutes);
@@ -94,7 +111,7 @@ app.post('/api/forgot-password',async (req,res)=>{
         if (passwordResetToken == null){
             res.status(500).json({msg: "No user Exists with this email"});
         }else{
-            const passwordResetLink = req.protocol + '://' + req.get('host') + "/api/reset-password?token="+passwordResetToken;
+            const passwordResetLink = req.protocol + '://' + req.get('host') + "/reset-password?token="+passwordResetToken;
             sendPasswordResetEmail(email, passwordResetLink);
 
             res.status(200).json({ message: 'Password reset email sent' });
@@ -170,4 +187,60 @@ app.post('/api/2fa/verify',async (req, res)=>{
 
 });
 
+app.put('/api/venues', async (req, res)=>{
+   try{
+    const updatedVenue = req.body;
+    await db.promise().query(
+      'UPDATE ignite.venues SET sport_name = ?, venue_name = ?, location = ?, reservation_type = ?, address = ?, status = ?, dates = ?, times = ? WHERE venue_id = ?',
+      [updatedVenue.sport_name, updatedVenue.venue_name, updatedVenue.location, updatedVenue.reservation_type, 
+        updatedVenue.address, updatedVenue.status, updatedVenue.dates, updatedVenue.times, updatedVenue.venue_id]);
+
+    res.status(200).json(updatedVenue);
+   }catch(error){
+     res.status(400).json({message:'internal server error'});
+   }
+});
+
+app.post('/api/reservations', async (req, res)=>{
+  try{
+    var {sorting, venue, date} = req.body;
+
+    const defaultSorting = 0;
+    const defaultVenue = 'ALL';
+
+    const usedSorting = sorting === undefined ? defaultSorting : sorting;
+    const usedVenue = venue === undefined ? defaultVenue : venue;
+
+    if(date === undefined){
+      const currentDate = new Date();
+      const year = currentDate.getFullYear();
+      const month = currentDate.getMonth() + 1; 
+      const day = currentDate.getDate();
+
+      // Format the date as "YYYY-MM-DD" to match the SQL DATE data type
+      date = `${year}-${month < 10 ? '0' : ''}${month}-${day < 10 ? '0' : ''}${day}`;
+    }
+    const reservationsQuery = `SELECT * FROM ignite.reservations WHERE reservation_date >= ?${
+      usedVenue === 'ALL' ? '' : ' AND venue LIKE ?'
+    }`;
+    
+    const venuePattern = usedVenue === 'ALL' ? '%' : `%${usedVenue}%`;
+
+    const queryParams = usedVenue === 'ALL' ? [date] : [date, venuePattern];
+    const [reservations] = await db.promise().query(reservationsQuery, queryParams);
+    console.log(reservations)
+
+    const venueIds = [];
+    for(var i=0;i<reservations.length;i++){
+      venueIds.push(reservations[i].venue_id);
+    }
+    const venuesQuery = `SELECT * FROM ignite.venues WHERE venue_id IN (?) AND status = ? ORDER BY dates ${usedSorting < 1 ? 'ASC' : 'DESC'}`;
+    const [venuesList] = await db.promise().query(venuesQuery,[venueIds,'Open']);
+    
+    res.status(200).json(venuesList);
+  }catch(error){
+    console.log(error);
+    res.status(500).json({message:'internal server error'});
+  }
+});
 module.exports = app;
