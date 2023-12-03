6ebc3cbd093eb8c41dc11940ea4bba5fb415cdfd
gholloway32 <97569235+gholloway32@users.noreply.github.com>
2023-12-03 18:06:22 -0500

    Payment2

--- venuemanagement/src/components/Payment.js
+++ venuemanagement/src/components/Payment.js
@@ -3,7 +3,7 @@ import logo from "../assets/logo.png"
 import { Button } from "./Button";
 import NavBar from "./navBar";
 import { useNavigate } from "react-router-dom";
-/* http://localhost:8080/payment?venueid=1&reservation=2023-12-01%2020:09:00&reservationid=2*/ 
+
 const Paymethod = () => {
     const paymentParams = new URLSearchParams(window.location.search);
     const reservationID = paymentParams.get('reservationid');
